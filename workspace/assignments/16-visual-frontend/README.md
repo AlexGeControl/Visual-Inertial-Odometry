85621c322754e4d5a8073deb37150f54678a32ce
Yao Ge <alexgecontrol@qq.com>
2020-08-10 14:17:43 +0800

    set up homework course 6.

---
+++ workspace/assignments/16-visual-frontend/README.md
@@ -0,0 +1,31 @@
+# Visual Inertial Odometry: Backend Solver -- 从零开始手写VIO: Backend Solver
+
+This is the solution of Assignment 06 of Hands on VIO from [深蓝学院](https://www.shenlanxueyuan.com/course/247).
+
+深蓝学院从零开始手写VIO第6节Visual Frontend答案. 版权归深蓝学院所有. 请勿抄袭.
+
+---
+
+## Solutions
+
+---
+
+### 1. Prove the Optimality of the Solution to Eq. 15
+### 1. 证明下列策略为式15的最优解
+
+<img src="doc/problem-statement/assignment-01.png" alt="Problem Statement, Assignment 01" width="%100">
+
+---
+
+### 2. Implement the Triangulation for Feature Points and Pass the Simulation Test
+### 2. 完成特征点三角化代码, 并通过仿真测试
+
+---
+
+### 3. Visualize the Ratio Between the Smallest and the Second Smallest Singular Value as Measurement Noise Changes
+### 3. 请对测量值加上不同噪声, 观察最小奇异值与第二小奇异值之间的比例变化, 并绘制比例变化的曲线
+
+---
+
+### 4. Visualize the Ratio Between the Smallest and the Second Smallest Singular Value as Measurement Window Expands
+### 4. 固定噪声方差参数, 将观测图像扩展为多帧, 观察最小奇异值和第二小奇异值之间的比例变化, 并绘制比例变化的曲线
---
+++ workspace/assignments/16-visual-frontend/triangulate.cpp
@@ -0,0 +1,72 @@
+//
+// Created by hyj on 18-11-11.
+//
+#include <iostream>
+#include <vector>
+#include <random>  
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Eigenvalues>
+
+struct Pose
+{
+    Pose(Eigen::Matrix3d R, Eigen::Vector3d t):Rwc(R),qwc(R),twc(t) {};
+    Eigen::Matrix3d Rwc;
+    Eigen::Quaterniond qwc;
+    Eigen::Vector3d twc;
+
+    Eigen::Vector2d uv;    // 这帧图像观测到的特征坐标
+};
+int main()
+{
+
+    int poseNums = 10;
+    double radius = 8;
+    double fx = 1.;
+    double fy = 1.;
+    std::vector<Pose> camera_pose;
+    for(int n = 0; n < poseNums; ++n ) {
+        double theta = n * 2 * M_PI / ( poseNums * 4); // 1/4 圆弧
+        // 绕 z轴 旋转
+        Eigen::Matrix3d R;
+        R = Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitZ());
+        Eigen::Vector3d t = Eigen::Vector3d(radius * cos(theta) - radius, radius * sin(theta), 1 * sin(2 * theta));
+        camera_pose.push_back(Pose(R,t));
+    }
+
+    // 随机数生成 1 个 三维特征点
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> xy_rand(-4, 4.0);
+    std::uniform_real_distribution<double> z_rand(8., 10.);
+    double tx = xy_rand(generator);
+    double ty = xy_rand(generator);
+    double tz = z_rand(generator);
+
+    Eigen::Vector3d Pw(tx, ty, tz);
+    // 这个特征从第三帧相机开始被观测，i=3
+    int start_frame_id = 3;
+    int end_frame_id = poseNums;
+    for (int i = start_frame_id; i < end_frame_id; ++i) {
+        Eigen::Matrix3d Rcw = camera_pose[i].Rwc.transpose();
+        Eigen::Vector3d Pc = Rcw * (Pw - camera_pose[i].twc);
+
+        double x = Pc.x();
+        double y = Pc.y();
+        double z = Pc.z();
+
+        camera_pose[i].uv = Eigen::Vector2d(x/z,y/z);
+    }
+    
+    /// TODO::homework; 请完成三角化估计深度的代码
+    // 遍历所有的观测数据，并三角化
+    Eigen::Vector3d P_est;           // 结果保存到这个变量
+    P_est.setZero();
+    /* your code begin */
+ 
+    /* your code end */
+    
+    std::cout <<"ground truth: \n"<< Pw.transpose() <<std::endl;
+    std::cout <<"your result: \n"<< P_est.transpose() <<std::endl;
+    // TODO:: 请如课程讲解中提到的判断三角化结果好坏的方式，绘制奇异值比值变化曲线
+    return 0;
+}
